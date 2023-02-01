9ef110d02c963474fe161dac4d7405e135c6eda1
Pab-83 <gabrielpablo8344@gmail.com>
2023-02-01 17:51:56 -0300

    add word selector

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello World")
+import random
+
+
+def word_extracting():
+    stop_point = random.randint(1, 201)
+    counter = 0
+    with open("words.txt", "rt") as words:
+        for word in words:
+            counter += 1
+            if counter == stop_point:
+                return word
+
+
+
