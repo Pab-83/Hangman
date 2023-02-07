b8de64d047bd807bc0d6129d3959f385941c7196
Pab-83 <gabrielpablo8344@gmail.com>
2023-02-07 16:52:53 -0300

    changes

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import random
+from string import ascii_lowercase
 
 
 def word_extracting():
@@ -6,9 +7,18 @@ def word_extracting():
     counter = 0
     with open("words.txt", "rt") as words:
         for word in words:
+            clean_word = word.strip()
             counter += 1
             if counter == stop_point:
-                return word
+                return clean_word
 
+
+word = word_extracting()
+word_letters = [letter for letter in word]
+
+alphabet = list(ascii_lowercase)
+game_on = True
+
+print(word_letters)
 
 
