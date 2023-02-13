bfd36e560c75de935dcd9edb52b9987fc7a28ad6
Pab-83 <gabrielpablo8344@gmail.com>
2023-02-13 17:08:47 -0300

    add comments and complete ways of winning

--- main.py
+++ main.py
@@ -22,23 +22,29 @@ game_on = True
 
 
 while game_on:
+    print(word)
     print(f"You have {tries} tries")
     print(hidden_word)
     guess_letter = input("Guess a letter: ")
 
-    if guess_letter == word:
+    #Check if player tries to guess the word
+    if guess_letter == word or hidden_word == word:
         print("You Win!")
         tries = 0
-
+    
     elif len(guess_letter) > 1:
         print(f"Sorry the word was {word}, you lost")
         tries = 0
 
-    elif guess_letter in word_letters and guess_letter in alphabet:
-        alphabet.remove(guess_letter)
+    #Check if the user guess is in the word and if is not a repeated letter
+    if guess_letter in word_letters and guess_letter in alphabet:
         for x in range(len(word_letters)):
             if word_letters[x] == guess_letter:
                 hidden_word[x] = guess_letter
+        if hidden_word == word_letters:
+            print("You Win!")
+            print(word)
+            tries = 0
 
     elif guess_letter not in alphabet and len(guess_letter) == 1:
         print("You already try that letter, try again")
@@ -48,6 +54,11 @@ while game_on:
         if tries == 0:
             print(f"Sorry the word was {word}, you lost")
 
+    #Remove guessed letter to avoid repetition
+    if guess_letter in alphabet:
+        alphabet.remove(guess_letter)
+
+    # Game over cases
     if tries == 0:
         while True:
             try_again = input("Do you want to try again? (y/n)").lower()
